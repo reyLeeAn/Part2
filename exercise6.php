5b7d501eba354555989a29a1be7e60686130ac16
reyLeeAn <113161547+reyLeeAn@users.noreply.github.com>
2022-10-02 21:24:39 +0800

    Add files via upload

---
+++ exercise6.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Exercise 7</title>
+</head>
+<body>
+
+<?php
+
+for($i = 1; $i<=12; $i++){
+	print("$i * $i = " . ($i*$i) . "<br>");
+}
+
+?>
+
+</body>
+</html>
